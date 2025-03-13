94b098b3f04c517dc342fa0365041a61303c7334
tbeamis <tbeamis@uwo.ca>
2025-03-13 18:20:41 -0400

    Created a function to convert from place id to lat lng

--- GeoCode_Geocoding.py
+++ GeoCode_Geocoding.py
@@ -4,24 +4,24 @@ class Geocoding:
     API_KEY = os.getenv('geocode')
     
     base_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
-
-    def __init__(self, address='1600 Pennsylvania Avenue NW, Washington, DC',places_id='aaaaaa'):
+    #Creating constructor for all of the variables needed in the class
+    def __init__(self, address='1600 Pennsylvania Avenue NW, Washington, DC',place_id='ChIJ0SvEag7uLogRhsBvcb7vTO0'):
         self.address = address
-        self.places_id=places_id
+        self.place_id=place_id
         self.coordpair = [0, 0]
         self.params = {
             'key': self.API_KEY,
             'address': self.address
         }
         self.idparams={'key': self.API_KEY,
-                       'places_id': self.places_id}
+                       'place_id': self.place_id}
 
-
+    #creating setter methods to set necessary Parameters in OOP
     def setParams(self, address):
         self.params['address'] = address
-    def setIdParams(self,places_id):
+    def setIdParams(self,place_id):
         self.idparams={'key':self.API_KEY,
-                       'places_id':self.places_id}
+                       'place_id':place_id}
 
     def get_coord(self):
         response = requests.get(self.base_url, params=self.params).json()
@@ -38,13 +38,16 @@ class Geocoding:
             return None
     def get_placeidtocoord(self):
         idresponse=requests.get(self.base_url,params=self.idparams).json()
+        print(self.idparams)
         if idresponse['status']=='OK':
-            print(idresponse['results'])
+            print(idresponse['results'][0]['formatted_address'])
+            print(idresponse['results'][0]['geometry']['location'])
         else:
-            print("Error:", response['status'])
+            print("Error:", idresponse['status'])
             return None
 
 
 class Main:
     Alex = Geocoding()
     Alex.get_coord()
+    Alex.get_placeidtocoord()
