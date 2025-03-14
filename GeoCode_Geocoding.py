21e02e33e1842e0bff84833b97484547657a5015
tbeamis <tbeamis@uwo.ca>
2025-03-14 14:25:22 -0400

    Created syncro with address vali and added air

--- GeoCode_Geocoding.py
+++ GeoCode_Geocoding.py
@@ -7,7 +7,7 @@ class Geocoding:
     
     base_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
     #Creating constructor for all of the variables needed in the class
-    def __init__(self, address='1600 Pennsylvania Avenue NW, Washington, DC',place_id='ChIJ0SvEag7uLogRhsBvcb7vTO0'):
+    def __init__(self, address='3018 Yonge Street, Toronto, ON M4N 0A5',place_id='ChIJ0SvEag7uLogRhsBvcb7vTO0'):
         self.address = address
         self.place_id=place_id
         self.coordpair = [0, 0]
@@ -18,14 +18,13 @@ class Geocoding:
         self.idparams={'key': self.API_KEY,
                        'place_id': self.place_id}
 
-    #creating setter methods to set necessary Parameters in OOP
-    def setParams(self, address):
-        self.params['address'] = address
+    #creating setter methods to set necessary Parameters in OOP        
     def setIdParams(self,place_id):
         self.idparams={'key':self.API_KEY,
                        'place_id':place_id}
 
-    def get_coord(self):
+    def get_coord(self,address):
+        self.params['address'] = address
         response = requests.get(self.base_url, params=self.params).json()
         if response['status'] == 'OK':
             location = response['results'][0]['geometry']['location']
@@ -37,7 +36,7 @@ class Geocoding:
             return self.coordpair
         else:
             print("Error:", response['status'])
-            return None
+            return self.coordpair
     def get_placeidtocoord(self):
         idresponse=requests.get(self.base_url,params=self.idparams).json()
         print(self.idparams)
