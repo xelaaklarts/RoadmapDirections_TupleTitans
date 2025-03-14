ffc21dcaf95f0d48dc832fb3a561ea1cfedd7c1f
xelaaklarts <astralka@uwo.ca>
2025-03-13 23:54:06 -0400

    Removed Imformation

--- dv_testfile.py
+++ dv_testfile.py
@@ -6,7 +6,5 @@ exitValue = False
 print(dv_ui(exitValue, 0))
 print(dv_ui(exitValue, 1))
 
-API_KEY = "AIzaSyBRJa0So36C563ifjSXqX1MjyKasteshAU"
+API_KEY = "NO_API_KEY_TO_SEE_HERE"
 URL = f"https://addressvalidation.googleapis.com/v1:validateAddress?key={API_KEY}"
-
-
